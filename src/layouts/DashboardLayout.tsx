bf3202231b2a23a351146ce30ba334213b116dd8
Jarias <jarias7604@gmail.com>
2026-01-25 23:26:26 -0500

    style: reduce layout top padding to move content higher

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -16,8 +16,8 @@ export default function DashboardLayout() {
             {/* Main Content Area */}
             {/* Added pb-20 for mobile bottom nav spacing */}
             <div className="md:pl-64 flex flex-col min-h-screen transition-all duration-300 pb-20 md:pb-0">
-                {/* Symmetric vertical spacing: 64px (pt-16) to ensure the card feels perfectly centered */}
-                <main className="flex-1 w-full max-w-[1580px] mx-auto px-4 md:px-8 md:pt-16 md:pb-16">
+                {/* Reduced symmetric vertical spacing: 32px (pt-8) for a tighter fit */}
+                <main className="flex-1 w-full max-w-[1580px] mx-auto px-4 md:px-8 md:pt-8 md:pb-12">
                     <Outlet />
                 </main>
             </div>
